c33dfe4063b7bd2fe5cd05689ca6a6596567c756
faisalsabier1 <87351802+faisalsabier1@users.noreply.github.com>
2021-07-13 08:56:57 +0300

    Update ServoMotor.ino

--- ServoMotor.ino
+++ ServoMotor.ino
@@ -1,17 +1,17 @@
-#include <Servo.h>
-Servo servo1;
+#include <Servo.h> //Including servo library 
+Servo servo1; //Creating copy from the library for 5 servo motors
 Servo servo2;
 Servo servo3;
 Servo servo4;
 Servo servo5;
-int min = 480;
+int min = 480; // setting the min and max value to make sure that the servo motor reach to the desired angle
 int max = 2500;
-int potpin = 0;
-int val;
+int potpin = 0; // varible for the values of the resistors pin 0
+int val; //varible for 
 
 void setup()
 {
-  servo1.attach(7,min,max);
+  servo1.attach(7,min,max); //seting the values for the adruino  
   servo2.attach(6,min,max);
   servo3.attach(5,min,max);
   servo4.attach(4,min,max);
@@ -19,13 +19,13 @@ void setup()
 }
 void loop()
 {
-  val = analogRead(potpin);            
-  val = map(val, 0, 1023, 0, 90);
+  val = analogRead(potpin); //read the value from the varible resistor (scaling)           
+  val = map(val, 0, 1023, 0, 90); // this part is setting the resistor value and the maximum angle for the variable resistor (0-90 degrees)
   
-  servo1.write(val);
+  servo1.write(val); // sending the values to each servo 
   servo2.write(val);
   servo3.write(val);
   servo4.write(val);
   servo5.write(val);
-  delay(2000); }
+  delay(2000); } //delay after each movment 
   
